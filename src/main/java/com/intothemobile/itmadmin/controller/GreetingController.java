ca6868b01af3b9322c133d54b212d04a5d2eae08
Donggill Shin <dg.shin@intothemobile.com>
2019-09-11 14:14:06 +0900

    Swagger 설정 추가

--- src/main/java/com/intothemobile/itmadmin/controller/GreetingController.java
+++ src/main/java/com/intothemobile/itmadmin/controller/GreetingController.java
@@ -2,6 +2,8 @@ package com.intothemobile.itmadmin.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,13 @@ import com.intothemobile.itmadmin.model.Greeting;
 public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	
+	private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		logger.debug("greeting");
+		logger.debug(name);
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
