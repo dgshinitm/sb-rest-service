00833efd7c06c698fadb6a62e627b353628dd80f
Donggill Shin <dg.shin@intothemobile.com>
2019-09-24 16:17:48 +0900

    Swagger 설정 수정

--- src/main/java/com/intothemobile/itmadmin/controller/GreetingController.java
+++ src/main/java/com/intothemobile/itmadmin/controller/GreetingController.java
@@ -5,10 +5,15 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.intothemobile.itmadmin.model.Greeting;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class GreetingController {
@@ -17,7 +22,17 @@ public class GreetingController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
-	@RequestMapping("/greeting")
+	@ApiOperation(
+			httpMethod = "GET"
+			, value = "Rest API greeting"
+			, notes = "Return greeting message"
+			, nickname = "greeting"
+			, response = Greeting.class
+	)
+	@ApiImplicitParams({
+	    @ApiImplicitParam(name = "name", value = "Name", defaultValue="World", required = false, dataType = "String", paramType="query")
+    })
+	@RequestMapping(path = "/greeting", method = RequestMethod.GET)
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		logger.debug("greeting");
 		logger.debug(name);
