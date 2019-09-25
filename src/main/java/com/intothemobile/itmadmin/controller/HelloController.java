baae5b82eb230281d04d43d2dc58d5e6a1a99bf7
Donggill Shin <dg.shin@intothemobile.com>
2019-09-25 14:20:03 +0900

    Web controller 추가

---
+++ src/main/java/com/intothemobile/itmadmin/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.intothemobile.itmadmin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("name", "Donggill");
+		return "hello";
+	}
+}
